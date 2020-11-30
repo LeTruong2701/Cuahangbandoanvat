ccdca7e8cab123d554152838c0c3c3c6b60ad703
LeTruong2701 <72636049+LeTruong2701@users.noreply.github.com>
2020-11-30 21:34:03 +0700

    Add project files.

---
+++ Cuahangbandoanvat/BUS/HangHoaBUS.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Cuahangbandoanvat.DAL;
+
+namespace Cuahangbandoanvat.BUS
+{
+    class HangHoaBus
+    {
+        private HangHoaDAL hhDAL = new HangHoaDAL();
+
+        public void Them(string maHH, string tenHH,string loaiHH, double giaban)
+        {
+            hhDAL.Them(maHH, tenHH,loaiHH,giaban);
+        }
+
+        public void Sua(string maHH, string tenHH,string loaiHH,double giaban)
+        {
+            hhDAL.Sua(maHH, tenHH,loaiHH,giaban);
+        }
+
+        public void Xoa(string maHH)
+        {
+            hhDAL.Xoa(maHH);
+        }
+
+        public List<String> LayDanhSach()
+        {
+            return hhDAL.Laydanhsach();
+        }
+        //public void Timkiem(string maHH)
+        //{
+        //    hhDAL.Timkiem(maHH);
+        //}
+    }
+}
---
+++ Cuahangbandoanvat/BUS/KhachhangBUS.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cuahangbandoanvat.DAL;
+
+namespace Cuahangbandoanvat.BUS
+{
+    class KhachhangBUS
+    {
+        private KhachHangDAL khDAL = new KhachHangDAL();
+        public List<String> LayDanhSach()
+        {
+            return khDAL.Laydanhsach();
+        }
+        public void Xoa(string maKH)
+        {
+            khDAL.Xoa(maKH);
+        }
+        public void Sua(string maKH, string tenKH, string diachiKH, int sdtKH)
+        {
+            khDAL.Sua(maKH, tenKH, diachiKH, sdtKH);
+        }
+        public void ThemKH(string maKH,string tenKH,string diachiKH,int sdtKH)
+        {
+            khDAL.ThemKH(maKH, tenKH, diachiKH, sdtKH);
+        }
+    }
+}
